6c3f3db7c227633525ed19be789e4a41a64078ff
MohamedAdel0301 <mosoliman33@gmail.com>
2023-10-26 14:44:42 -0700

    added basic navbar

---
+++ src/components/shared/navbar/CategoriesButton.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Link from "next/link"
+
+const NavigationButtons = () => {
+  return (
+    <button>
+      Categories
+    </button>
+  )
+}
+
+export default NavigationButtons
