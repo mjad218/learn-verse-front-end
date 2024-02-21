a7bf9f65320f422ea593b66c2da80111de7fa427
MohamedAdel0301 <mosoliman33@gmail.com>
2024-02-21 14:27:13 +0200

    minor cleanup

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -5,8 +5,6 @@ import { BsFacebook, BsYoutube, BsLinkedin, BsTwitter, BsInstagram } from 'react
 const Footer = () => {
 
   const icons = [BsFacebook, BsYoutube, BsLinkedin, BsTwitter, BsInstagram]
-  const formInputStyles = 'text-gray-600 w-[200px] rounded-md px-1 py-2'
-
 
   return (
     <footer className='w-full bg-[#0C090A] flex flex-col justify-center py-6 text-xs'>
@@ -41,7 +39,7 @@ const Footer = () => {
       </div>
       {/* Bottom Section */}
       <div className='self-center flex flex-col justify-center gap-2'>
-        <p className='text-white font-roboto text-sm'>© 2023 LearnVerse. All rights reserved.</p>
+        <p className='text-white font-roboto text-sm'>© 2024 LearnVerse. All rights reserved.</p>
         <div className='flex gap-6 self-center'>
           {icons.map((icon) => (<FooterIcon Icon={icon} key={icon.name} />))}
         </div>
