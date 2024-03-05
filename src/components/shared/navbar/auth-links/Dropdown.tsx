a0a91bf9c021a33d0d51a208bc32655794a727b7
MohamedAdel0301 <mosoliman33@gmail.com>
2024-03-06 01:48:50 +0200

    added dropdown styles

--- src/components/shared/navbar/auth-links/Dropdown.tsx
+++ src/components/shared/navbar/auth-links/Dropdown.tsx
@@ -7,10 +7,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import DropdownAvatar from "./DropDownAvatar";
-
+import Link from "next/link";
 interface Props {
   user: { name: string; email: string };
 }
+
+const menuItemStyle =
+  "cursor-pointer text-lg font-medium focus:bg-[#2563EB] focus:rounded focus:text-white";
 
 const Dropdown = ({ user }: Props) => {
   return (
@@ -20,10 +23,20 @@ const Dropdown = ({ user }: Props) => {
           <DropdownAvatar />
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+          <DropdownMenuLabel className="text-lg font-medium">
+            {user.email}
+          </DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Logout</DropdownMenuItem>
+          <Link href="/settings">
+            <DropdownMenuItem className={menuItemStyle}>
+              Settings
+            </DropdownMenuItem>
+          </Link>
+          <Link href="/login">
+            <DropdownMenuItem className={menuItemStyle}>
+              Logout
+            </DropdownMenuItem>
+          </Link>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
