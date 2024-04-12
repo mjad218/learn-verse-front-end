3a256353f5c814b00514dfba155fe2c733d92cf3
MohamedAdel0301 <mosoliman33@gmail.com>
2024-04-12 14:15:22 +0200

    added rating indicator

--- src/app/search/_components/Stars.tsx
+++ src/app/search/_components/Stars.tsx
@@ -1,8 +1,9 @@
 // @ts-ignore
+import { IoIosRadioButtonOn, IoIosRadioButtonOff } from "react-icons/io";
 import ReactStars from "react-stars";
 import { parseAsFloat, useQueryState } from "nuqs";
 import { useRouter } from "next/navigation";
-import { Grid, Text, Box } from "@radix-ui/themes";
+import { Grid, Text, Flex } from "@radix-ui/themes";
 
 const Stars = ({ value }: { value: number }) => {
   const router = useRouter();
@@ -16,13 +17,18 @@ const Stars = ({ value }: { value: number }) => {
   return (
     <Grid
       onClick={changeRating}
-      columns={"5"}
+      columns={"7"}
       className="place-items-center hover:cursor-pointer hover:bg-neutral-200/80"
     >
-      <Box style={{ gridColumn: " 1/3" }}>
-        <ReactStars edit={false} size={24} value={value} />
-      </Box>
-      <Text size={"4"} className="font-thin" style={{ gridColumn: "3 / 6" }}>
+      <Flex gap="2" pl={"2"} align={"center"} style={{ gridColumn: " 1/4" }}>
+        {rating == value ? (
+          <IoIosRadioButtonOn size={20} />
+        ) : (
+          <IoIosRadioButtonOff size={20} />
+        )}
+        <ReactStars edit={false} size={20} value={value} />
+      </Flex>
+      <Text size={"4"} className="font-thin" style={{ gridColumn: "4 / 8" }}>
         {value} Stars {value == 5 ? "" : "& above"}
       </Text>
     </Grid>
