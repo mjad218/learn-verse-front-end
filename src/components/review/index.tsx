833e0ec86c59beab6e65b7b3d5efff0b077bb1e4
Mohamed Gad <mohamedgad149@gmail.com>
2024-04-24 02:14:01 +0200

    half=true React Stars

--- src/components/review/index.tsx
+++ src/components/review/index.tsx
@@ -10,7 +10,12 @@ export const SingleReview = (props: IProps) => {
     <div className="flex flex-col gap-1 rounded-xl bg-slate-300 p-2">
       <h1>{props?.review?.student?.firstName}</h1>
       <p>{props?.review?.content}</p>
-      <ReactStars edit={false} size={20} value={props?.review?.n_stars ?? 0} />
+      <ReactStars
+        half
+        edit={false}
+        size={20}
+        value={props?.review?.n_stars ?? 0}
+      />
       stars,
     </div>
   );
--- src/components/single-course-page/course-details/course-meta-data/index.tsx
+++ src/components/single-course-page/course-details/course-meta-data/index.tsx
@@ -9,7 +9,12 @@ export const CourseMetaData = (props: IProps) => {
   return (
     <div className="flex flex-col flex-wrap justify-between gap-3 lg:flex-row">
       {/* Just dummy emojis, will get replaced later */}
-      <ReactStars edit={false} size={20} value={props.course?.rating ?? 0} />
+      <ReactStars
+        half
+        edit={false}
+        size={20}
+        value={props.course?.rating ?? 0}
+      />
       stars,
       <span className="w-fit">{props.course?.nStudents ?? 0} Students</span>
     </div>
