5203973a8c2f6122b1eb76672de09f8e22cc7c05
Mohamed Gad <mohamedgad149@gmail.com>
2024-04-24 02:12:25 +0200

    fix hydration errors

--- src/components/review/index.tsx
+++ src/components/review/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Review } from "@/types/review.type";
 import ReactStars from "react-stars";
 
@@ -9,14 +10,8 @@ export const SingleReview = (props: IProps) => {
     <div className="flex flex-col gap-1 rounded-xl bg-slate-300 p-2">
       <h1>{props?.review?.student?.firstName}</h1>
       <p>{props?.review?.content}</p>
-      <span>
-        <ReactStars
-          edit={false}
-          size={20}
-          value={props?.review?.n_stars ?? 0}
-        />
-        stars,
-      </span>
+      <ReactStars edit={false} size={20} value={props?.review?.n_stars ?? 0} />
+      stars,
     </div>
   );
 };
--- src/components/single-course-page/course-details/course-meta-data/index.tsx
+++ src/components/single-course-page/course-details/course-meta-data/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Course } from "@/types/course.type";
 import ReactStars from "react-stars";
 
@@ -8,11 +9,8 @@ export const CourseMetaData = (props: IProps) => {
   return (
     <div className="flex flex-col flex-wrap justify-between gap-3 lg:flex-row">
       {/* Just dummy emojis, will get replaced later */}
-      <p className="flex w-fit flex-wrap gap-2">
-        <ReactStars edit={false} size={20} value={props.course?.rating ?? 0} />
-        stars,
-        {/* <span className="w-fit">25 Ratings</span> */}
-      </p>
+      <ReactStars edit={false} size={20} value={props.course?.rating ?? 0} />
+      stars,
       <span className="w-fit">{props.course?.nStudents ?? 0} Students</span>
     </div>
   );
