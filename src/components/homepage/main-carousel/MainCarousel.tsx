5dc9fe3478fc91051d3970608458ae7871eb529b
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-14 11:36:47 +0300

    added navigation to main carousel

--- src/components/homepage/main-carousel/MainCarousel.tsx
+++ src/components/homepage/main-carousel/MainCarousel.tsx
@@ -1,30 +1,55 @@
+"use client";
+
 import { Row } from "@/components/shared/row";
 import MainCarouselCard from "./MainCarouselCard";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
+import { useState, useEffect } from "react";
+import { type CarouselApi } from "@/components/ui/carousel";
+import { Button } from "@/components/ui/button";
+
 const MainCarousel = () => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) return;
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
-    <Row className="my-4 ">
-      <Carousel className="relative mx-auto w-[calc(100%-66px)] ">
+    <Row className="my-4">
+      <Carousel className="relative mx-auto" setApi={setApi}>
         <CarouselContent>
-          <CarouselItem className="w-full">
-            <MainCarouselCard />
-          </CarouselItem>
-          <CarouselItem className="w-full">
-            <MainCarouselCard />
-          </CarouselItem>
-          <CarouselItem className="w-full">
-            <MainCarouselCard />
-          </CarouselItem>
+          {[1, 2, 3].map((_, index) => (
+            <CarouselItem className="w-full" key={`slider${index}`}>
+              <MainCarouselCard />
+            </CarouselItem>
+          ))}
         </CarouselContent>
-        <CarouselPrevious className="border-2 border-gray-600" />
-        <CarouselNext className="border-2 border-gray-600" />
       </Carousel>
+      <div className="my-2 flex justify-center gap-1">
+        {Array.from(Array(count).keys()).map((i) => (
+          <Button
+            key={`{dot-count-${i}}`}
+            className={`h-0.5 rounded-full  ${
+              i === current - 1
+                ? "bg-black hover:bg-black"
+                : "bg-neutral-600/60"
+            }`}
+            onClick={() => api?.scrollTo(i)}
+          />
+        ))}
+      </div>
     </Row>
   );
 };
--- src/components/homepage/main-carousel/MainCarouselCard.tsx
+++ src/components/homepage/main-carousel/MainCarouselCard.tsx
@@ -1,26 +1,29 @@
+"use client";
 import {
   Card,
   CardTitle,
   CardHeader,
   CardDescription,
 } from "@/components/ui/card";
-
 import MainCarouselAvatar from "./MainCarouselAvatar";
+
 const MainCarouselCard = () => {
+
+
   return (
-    <div>
+    <div className="font-roboto">
       <Card
         className="relative mx-auto h-96 w-full
        cursor-pointer bg-[url('/assets/TempImages/TempCourse.jpg')] bg-cover"
       >
-        <CardHeader className="absolute left-0 right-0 top-8 bg-zinc-800/50">
+        <CardHeader className="absolute left-0 right-0 top-8 bg-zinc-800/80">
           <div className="flex flex-col items-center justify-center gap-4">
             <CardTitle className="font-bold text-gray-200">
               Learn JavaScript by doing
             </CardTitle>
             <MainCarouselAvatar />
             <CardDescription className="text-2xl font-medium text-gray-200">
-              {"Instructor"}
+              {"<Instructor>"}
             </CardDescription>
           </div>
         </CardHeader>
