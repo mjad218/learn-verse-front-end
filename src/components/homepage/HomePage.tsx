55f9e9643313b117feaffb4f024bf7897534ddce
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-15 07:30:26 +0300

    added secondary hero section

--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -1,3 +1,4 @@
+import ContentSection from "./content-section";
 import HeroCarousel from "./hero-carousel/MainCarousel";
 import MainCarousel from "./main-carousel/MainCarousel";
 
@@ -6,10 +7,11 @@ const HomePage = () => {
     <>
       <HeroCarousel />
       <div className="my-4">
-        <h1 className="font-roboto mx-auto my-4 max-w-fit text-3xl font-medium">
+        <h1 className="mx-auto my-4 max-w-fit font-roboto text-3xl font-medium">
           Featured Courses
         </h1>
         <MainCarousel />
+        <ContentSection />
       </div>
     </>
   );
