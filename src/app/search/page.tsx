56d1325e7b1379a3bd0a5f9acadccacb47ed6b07
Mohamed Gad <mohamedgad149@gmail.com>
2024-06-17 18:17:37 +0300

    search page adjust flex basis

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -13,13 +13,13 @@ const ResultsMessage = dynamic(
 const SearchPage = () => {
   return (
     <Row>
-      <div className="flex flex-col gap-6 py-10 text-center text-xl lg:flex-row">
-        <div className="flex basis-1/3 flex-col">
+      <div className="flex flex-col gap-10 py-10 text-center text-xl lg:flex-row">
+        <div className="flex basis-3/12 flex-col">
           <SearchOptions />
         </div>
-        <div className="flex basis-2/3 flex-col gap-5">
+        <div className="flex basis-9/12 flex-col gap-5">
           <ResultsMessage />
-          <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,320px))] gap-3">
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,300px))] gap-3">
             {[1, 2, 3, 4, 5, 6].map((_, index) => (
               <CourseCard key={index} />
             ))}
