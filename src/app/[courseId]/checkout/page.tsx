7195e4b7e8f353eb735e09f10ac3c1d14c5b6c57
Mohamed Gad <mohamedgad149@gmail.com>
2024-06-20 20:49:36 +0300

    checkout get single course - access token

--- src/app/[courseId]/checkout/page.tsx
+++ src/app/[courseId]/checkout/page.tsx
@@ -1,5 +1,6 @@
 import CheckoutFormContainer from "@/components/single-course-page/course-checkout/CheckoutFormContainer";
 import { getSingleCourse } from "@/services/courses/single-course";
+import { cookies } from "next/headers";
 
 type Props = {
   params: { courseId: string };
@@ -7,7 +8,14 @@ type Props = {
 
 const page = async ({ params }: Props) => {
   const courseId = params.courseId;
-  const course = await getSingleCourse(courseId);
+  let token: string | null = "";
+  try {
+    const nextCookies = cookies();
+    token = nextCookies.get("token")?.value ?? null;
+    if (!token) throw "not logged in";
+  } catch (error) {}
+
+  const course = await getSingleCourse(courseId, token);
   return (
     <div className="my-4 flex items-center justify-center">
       <CheckoutFormContainer courseInfo={course} />
