ffe2be20eb18ace316b4ba243d4f5a83199d6598
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-20 13:01:12 +0300

    more style adjustments

--- src/components/homepage/join/index.tsx
+++ src/components/homepage/join/index.tsx
@@ -1,23 +1,22 @@
-import { Row } from "@/components/shared/row"
-import Link from "next/link"
+import { Row } from "@/components/shared/row";
+import Link from "next/link";
 
 export const JoinUs = () => {
   return (
-    <Row >
-     <div className="relative bg-accent4 py-16 px-20 rounded-t-3xl flex flex-col justify-center ">
-           
-     <h2 className="font-dmSerif text-4xl text-white mb-4">
-            Join LearnVerse! 
+    <Row>
+      <div className="relative flex flex-col justify-center rounded-t-3xl bg-accent4 px-20 py-16">
+        <h2 className="mb-4 font-dmSerif text-4xl text-white">
+          Join LearnVerse!
         </h2>
 
-<p className="text-white">
-Level up your knowledge!
-
-</p>
-<Link href={'/auth/register'} className="mt-10 text-slate-800 font-dmSerif bg-accent9 py-1 px-6 rounded-3xl w-fit"> 
-Sign Up
-</Link>
-     </div>
+        <p className="text-white">Level up your knowledge!</p>
+        <Link
+          href={"/auth/register"}
+          className="mt-10 w-fit rounded-3xl bg-black px-4 py-1 font-dmSerif text-white transition-transform delay-75 hover:scale-110"
+        >
+          Sign Up
+        </Link>
+      </div>
     </Row>
-  )
-}
+  );
+};
