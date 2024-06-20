199b42903bb8c63ddd8eb47b8a0ce1a6cf0494ec
Mohamed Gad <mohamedgad149@gmail.com>
2024-06-20 23:02:32 +0300

    account page styles

--- src/components/account-panel/index.tsx
+++ src/components/account-panel/index.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/app/auth/styleDefinitions";
 import { Button } from "@/components/ui/button";
 import { AccountDetailsSchema } from "@/components/side-panel/definitions";
+import { useState } from "react";
+import { Input } from "../ui/input";
 
 type AccountDetailsType = z.infer<typeof AccountDetailsSchema>;
 
@@ -22,120 +24,120 @@ const AccountPanel = () => {
     defaultValues: { username: "", email: "", firstname: "", lastname: "" },
   });
 
+  const [image, setImage] = useState<File | null | undefined>(null);
   const submitForm = async (data: FieldValues) => {
     console.log(data);
   };
-
+  //   {
+  //     "id": 3,
+  //     "userName": "gadoz11updated",
+  //     "password": "26508541updated",
+  //     "email": "myMailupdated",
+  //     "address": "myAddressupdated",
+  //     "firstName": "mohamedupdated",
+  //     "familyName": "gadupdated"
+  // }
   return (
-    <>
+    <div className="flex flex-col gap-8">
       <span className="font-roboto text-2xl font-semibold">Account</span>
-      <div className="mx-auto mt-4 w-[75%] pb-4">
+      <div className="mt-4 pb-4">
         <div className="flex flex-col items-center gap-2">
           <div className="flex flex-col gap-1 self-start">
-            <Button className="w-max bg-accent hover:bg-accent">
-              Choose File
-            </Button>
+            <Input
+              type="file"
+              onChange={(e) =>
+                setImage(e.target?.files?.length ? e.target?.files[0] : null)
+              }
+            />
             <div className="flex flex-col text-sm text-gray-700/60">
               <span>No file selected</span>
               <span>Maximum image size is 1MB</span>
             </div>
           </div>
-          <form className="w-full" onSubmit={() => handleSubmit(submitForm)}>
-            <div className="mx-auto flex w-[80%] flex-col gap-4">
-              <div className="flex gap-2">
-                <Label htmlFor="firstname" className={formInputLabelSettings}>
-                  First name
-                </Label>
-                <div className="flex w-full flex-col">
-                  <input
-                    type="text"
-                    id="firstname"
-                    placeholder="First Name"
-                    className={`${formInputStyle}`}
-                    {...register("firstname")}
-                  />
-                  {errors.firstname && (
-                    <span className="text-xs font-semibold text-red-700">
-                      {errors.firstname.message}
-                    </span>
-                  )}
-                </div>
-              </div>
-              <div className="flex items-center gap-2">
-                <Label htmlFor="lastname" className={formInputLabelSettings}>
-                  Last name
-                </Label>
-                <div className="flex w-full flex-col">
-                  <input
-                    type="text"
-                    id="lastname"
-                    placeholder="Last name"
-                    className={`${formInputStyle}`}
-                    {...register("lastname")}
-                  />
-                  {errors.lastname && (
-                    <span className="text-xs font-semibold text-red-700">
-                      {errors.lastname.message}
-                    </span>
-                  )}
-                </div>
-              </div>
-              <div className="flex items-center gap-2">
-                <Label
-                  htmlFor="username"
-                  className={`${formInputLabelSettings}`}
-                >
-                  Username
-                </Label>
-                <div className="flex w-full flex-col">
-                  <input
-                    type="text"
-                    id="username"
-                    placeholder="Username"
-                    className={`${formInputStyle}`}
-                    {...register("username")}
-                  />
-                  {errors.username && (
-                    <span className="text-xs font-semibold text-red-700">
-                      {errors.username.message}
-                    </span>
-                  )}
-                </div>
-              </div>
-              <div className="flex items-center justify-center gap-2">
-                <Label htmlFor="email" className={formInputLabelSettings}>
-                  Email
-                </Label>
-                <div className="flex w-full flex-col">
-                  <input
-                    type="text"
-                    id="email"
-                    placeholder="Email Address"
-                    className={`${formInputStyle}`}
-                    {...register("email")}
-                  />
-                  {errors.email && (
-                    <span className="text-xs font-semibold text-red-700">
-                      {errors.email.message}
-                    </span>
-                  )}
-                </div>
-              </div>
-              <Button
-                type="submit"
-                className={"self-center text-black disabled:border-gray-500"}
-                disabled={!isDirty}
-              >
-                Submit
-              </Button>
+
+          <form
+            className="flex w-full flex-col gap-4"
+            onSubmit={() => handleSubmit(submitForm)}
+          >
+            <div className="flex w-full flex-col">
+              <Label htmlFor="firstname" className={formInputLabelSettings}>
+                First name
+              </Label>
+              <input
+                type="text"
+                id="firstname"
+                placeholder="First Name"
+                className={`${formInputStyle}`}
+                {...register("firstname")}
+              />
+              {errors.firstname && (
+                <span className="text-xs font-semibold text-red-700">
+                  {errors.firstname.message}
+                </span>
+              )}
             </div>
+            <div className="flex flex-col items-center gap-2">
+              <Label htmlFor="lastname" className={formInputLabelSettings}>
+                Last name
+              </Label>
+              <input
+                type="text"
+                id="lastname"
+                placeholder="Last name"
+                className={`${formInputStyle}`}
+                {...register("lastname")}
+              />
+              {errors.lastname && (
+                <span className="text-xs font-semibold text-red-700">
+                  {errors.lastname.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col items-center gap-2">
+              <Label htmlFor="username" className={`${formInputLabelSettings}`}>
+                Username
+              </Label>
+              <input
+                type="text"
+                id="username"
+                placeholder="Username"
+                className={`${formInputStyle}`}
+                {...register("username")}
+              />
+              {errors.username && (
+                <span className="text-xs font-semibold text-red-700">
+                  {errors.username.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2">
+              <Label htmlFor="email" className={formInputLabelSettings}>
+                Email
+              </Label>
+              <input
+                type="text"
+                id="email"
+                placeholder="Email Address"
+                className={`${formInputStyle}`}
+                {...register("email")}
+              />
+              {errors.email && (
+                <span className="text-xs font-semibold text-red-700">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+            <Button
+              type="submit"
+              className={"self-center bg-accent text-white"}
+              disabled={!isDirty}
+            >
+              Submit
+            </Button>
           </form>
         </div>
-        <div className="flex flex-col items-center gap-4 pt-4">
-          <Button className={"text-red-500"}>Delete Account</Button>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 
