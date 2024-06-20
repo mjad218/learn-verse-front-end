d41cf0bdc413cd1d0f12d75733e22e1cb0ed646a
Mohamed Gad <mohamedgad149@gmail.com>
2024-06-20 22:12:44 +0300

    not found if no user

--- src/app/u/[slug]/page.tsx
+++ src/app/u/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { User } from "@/components/current-user/context";
 import { getUserById } from "@/services/users";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 type IProps = {
   params: { slug: string };
@@ -8,6 +9,7 @@ type IProps = {
 const UserPage = async (props: IProps) => {
   const user: User | null = await getUserById(props.params.slug);
 
+  if (!user) notFound();
   return (
     <div className="flex flex-col justify-center gap-6">
       <Image
