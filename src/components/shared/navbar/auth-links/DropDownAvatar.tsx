82991d5a4476b7540c89cea9a5a4237b0beec551
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-22 04:00:06 +0300

    drop down avatar change

--- src/components/shared/navbar/auth-links/DropDownAvatar.tsx
+++ src/components/shared/navbar/auth-links/DropDownAvatar.tsx
@@ -1,19 +1,28 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import AvatarPlaceholder from "@/../public/assets/home/avatar-placeholder.png";
 import Image from "next/image";
+import { fetchUserDetails } from "@/services/users";
+import { userDetailsType } from "@/types/user.type";
+import { cookies } from "next/headers";
 
-interface Props {
-  src?: string;
-  name?: string;
-}
+const DropdownAvatar = async () => {
+  let token: string | null = "";
+  try {
+    const nextCookies = cookies();
+    token = nextCookies.get("token")?.value ?? null;
+    if (!token) throw "not logged in";
+  } catch (error) {}
 
-const DropdownAvatar = ({ src, name }: Props) => {
+  const userDetails: userDetailsType = await fetchUserDetails(token);
+
   return (
     <div>
       <Avatar>
-        <AvatarImage src={src} />
+        <AvatarImage
+          src={`data:image/jpg;base64,${userDetails?.image ?? ""}`}
+        />
         <AvatarFallback>
-          {name ? name : <Image src={AvatarPlaceholder} alt="default avatar" />}
+          <Image src={AvatarPlaceholder} alt="avatar fallback" />
         </AvatarFallback>
       </Avatar>
     </div>
