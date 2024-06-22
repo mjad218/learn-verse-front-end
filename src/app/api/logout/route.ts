ce5adbf15234f65d41c94418438b7b76576af10a
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-22 23:56:18 +0300

    cookie store delete

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -6,15 +6,7 @@ export async function POST(request: NextRequest) {
   const cookieStore = cookies();
   request;
   try {
-    cookieStore.set({
-      name: "token",
-      value: "",
-      httpOnly: true,
-      path: "/",
-      maxAge: 0,
-      domain: new URL(API_URL).hostname,
-      sameSite: "lax",
-    });
+    cookieStore.delete("token");
   } catch (error) {
     return NextResponse.json(
       { success: false },
