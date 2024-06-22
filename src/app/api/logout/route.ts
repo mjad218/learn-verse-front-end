67f77dd7ae2394f8fb0ba349667cb3633791920d
Mohamed Gad <mohamedgad149@gmail.com>
2024-06-22 15:05:04 +0300

    log out route

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,7 +1,28 @@
+import { API_URL } from "@/constants/api";
 import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   const cookieStore = cookies();
-  cookieStore.set("token", "");
-  return Response.json({ success: true });
+  request;
+  try {
+    cookieStore.set({
+      name: "token",
+      value: "",
+      httpOnly: true,
+      path: "/",
+      maxAge: 0,
+      domain: new URL(API_URL).hostname,
+      sameSite: "lax",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false },
+      {
+        status: 500,
+        statusText: " failed",
+      },
+    );
+  }
+  return NextResponse.json({ success: true });
 }
