1f2d61074b2e4f6b16cbaa084cfca8724dc13c7f
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-22 11:43:40 +0300

    fixed build error

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,20 +1,9 @@
 import AccountPanel from "@/components/account-panel";
-import { fetchUserDetails } from "@/services/users";
-import { UserDetailsType } from "@/types/user.type";
-import { cookies } from "next/headers";
 
 const SettingsPage = async () => {
-  let token: string | null = "";
-  try {
-    const nextCookies = cookies();
-    token = nextCookies.get("token")?.value ?? null;
-    if (!token) throw "not logged in";
-  } catch (error) {}
-
-  const userDetails: UserDetailsType = await fetchUserDetails(token);
   return (
     <div>
-      <AccountPanel userDetails={userDetails} />
+      <AccountPanel />
     </div>
   );
 };
