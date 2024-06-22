cc27b4b11dbf1f8fec9969de06fe3b71d45a91f7
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-22 23:00:42 +0300

    fix credentials button style bug

--- src/components/authentication/_components/FormButton.tsx
+++ src/components/authentication/_components/FormButton.tsx
@@ -8,7 +8,7 @@ const FormButton = ({ text }: Props) => {
   return (
     <Button
       type="submit"
-      className="cursor-pointer self-center rounded-lg bg-accent px-4 font-roboto text-base/3"
+      className={`cursor-pointer self-center rounded-lg bg-accent px-4 font-roboto text-base/3 transition-transform delay-75 hover:scale-90 hover:bg-accent active:bg-accent`}
     >
       <div className="flex cursor-pointer items-center justify-center gap-1">
         <span>{text}</span>
