a73a2d006231471d014d93c24f1537d34927f027
MohamedAdel0301 <mosoliman33@gmail.com>
2024-06-23 09:53:41 +0300

    finalized single course video

--- src/components/single-course-page/side-bar/index.tsx
+++ src/components/single-course-page/side-bar/index.tsx
@@ -13,15 +13,19 @@ export const CourseSideBar = (props: IProps) => {
 
   return (
     <div className={props.className}>
-      {!isOwner && <h3 className="m-0 py-0 pb-3 text-2xl">Get Course</h3>}
       {isOwner && (
-        <Link href={`/${props.course?.id}/learning`}>Go to Course</Link>
+        <Link
+          className="w-full rounded-xl bg-accent3 px-2 py-3 text-base font-bold text-white"
+          href={`/${props.course?.id}/learning`}
+        >
+          Manage videos
+        </Link>
       )}
       <Link
         href={`/${props.course?.id}/checkout`}
         className="w-full rounded-xl bg-accent3 px-2 py-3 text-base font-bold text-white"
       >
-        Check out
+        Go to course
       </Link>
     </div>
   );
